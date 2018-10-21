fb3f563877578ff4c079d02105df1fd805417bfc
Anja Istenic <aistenic1@gmail.com>
2018-10-21 20:31:25 +0200

    changed spec file so it checks for word length correctly; implemented cnn word frequency method, working for some articles but not all

--- lib/cnn_word_frequency.rb
+++ lib/cnn_word_frequency.rb
@@ -5,14 +5,19 @@ def cnn_scraper(path)
   url = "https://edition.cnn.com/#{path}"
   document = Nokogiri::HTML(open(url).read)
   article_text = ''
-  document.search('.Article__body').each do |element|
-    article_text << element.text.strip
+  document.search('.Article__body').each { |x| article_text << x.text.strip }
+  article_text.gsub(/[\!?&\-"\.,\d]|\'(l{2}|s|d|re)/, '').downcase.split(' ')
+end
+
+def cnn_word_frequency(path, number_of_words)
+  article_text = cnn_scraper(path)
+  words = Hash.new(0)
+  article_text.each do |word|
+    words[word] += 1 if word.length > 4
   end
-  article_text.gsub(/[\!?&\-"\.,\d]|\'(l{2}|s|d)/, '').downcase.split(" ")
+  highest_reps = words.values.sort.reverse[0..number_of_words]
+  words.select { |_k, v| highest_reps.include? v }
 end
 
-def cnn_word_frequency(path)
-  p article_text = cnn_scraper(path)
-end
-
-cnn_word_frequency("travel/article/plane-sideways-landing-uk/index.html")
+# p cnn_word_frequency('travel/article/plane-sideways-landing-uk/index.html', 4)
+# p cnn_word_frequency('travel/article/laurie-woolever-anthony-bourdain-assistant/index.html?iid=ob_lockedrail_topeditorial', 3)
--- spec/cnn_word_frequency_spec.rb
+++ spec/cnn_word_frequency_spec.rb
@@ -1,12 +1,21 @@
 require 'cnn_word_frequency'
 
-describe "#cnn_word_frequency" do
+describe '#cnn_word_frequency' do
+  result_hash = cnn_word_frequency('travel/article/plane-sideways-landing-uk/index.html', 7)
 
-  it "should return a hash of cnn article words" do
-    expect(cnn_word_frequency("")).to be_a Hash
+  it 'should return a hash of cnn article words' do
+    expect(cnn_word_frequency('', 1)).to be_a Hash
   end
 
-  it "should return only words longer than 3 letters" do
-    hash.keys.each { |key| expect(key).to have_at_least(4).characters }
+  it 'should return only words longer than 4 letters' do
+    expect(words_length(result_hash)).not_to include(1, 2, 3, 4)
   end
+end
+
+def words_length(a_hash)
+  lengths = []
+  a_hash.each_key do |word|
+    lengths << word.length
+  end
+  lengths.uniq
 end
